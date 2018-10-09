2c45de4686de9b706e283c4a1ce152589bacec9d
InkoHX <inkohx@gmail.com>
2018-10-09 21:26:21 +0900

    init

---
+++ src/main/kotlin/inkohx/xyz/nukkit/kotori/Main.kt
@@ -0,0 +1,13 @@
+package inkohx.xyz.nukkit.kotori
+
+import cn.nukkit.plugin.PluginBase
+
+class Main : PluginBase() {
+    override fun onEnable() {
+        logger.info("test")
+    }
+
+    override fun onDisable() {
+        logger.info("test")
+    }
+}
