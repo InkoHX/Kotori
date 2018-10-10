8d2ca514dcb22038cbc3e819ca8f01e937cd776a
InkoHX <inkohx@gmail.com>
2018-10-11 05:10:25 +0900

    Update src/main/kotlin/inkohx/xyz/nukkit/kotori/serverlist/ServerListTask.kt

---
+++ src/main/kotlin/inkohx/xyz/nukkit/kotori/serverlist/ServerListTask.kt
@@ -0,0 +1,16 @@
+package inkohx.xyz.nukkit.kotori.serverlist
+
+import cn.nukkit.scheduler.PluginTask
+import inkohx.xyz.nukkit.kotori.Main
+
+class ServerListTask(owner: Main) : PluginTask<Main>(owner) {
+
+    private var time = 0
+
+    override fun onRun(currentTick: Int) {
+        if ((System.currentTimeMillis() / 1000L).toInt() - this.time > 60) {
+            this.time = (System.currentTimeMillis() / 1000L).toInt()
+            owner.api.updateTime()
+        }
+    }
+}
