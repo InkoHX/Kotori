0b722dd29f54fb89bebee6d90c54b6b4bca3ab5f
InkoHX <inkohx@gmail.com>
2018-10-10 14:20:18 +0900

    Add config

--- src/main/kotlin/inkohx/xyz/nukkit/kotori/EventListener.kt
+++ src/main/kotlin/inkohx/xyz/nukkit/kotori/EventListener.kt
@@ -5,10 +5,13 @@ import cn.nukkit.event.Listener
 import cn.nukkit.event.server.DataPacketReceiveEvent
 import cn.nukkit.network.protocol.LoginPacket
 
-class EventListener(internal var plugin: Main) : Listener {
+class EventListener(private val plugin: Main) : Listener {
     @EventHandler
     fun Receive(event: DataPacketReceiveEvent) {
         val packet = event.packet
-        if (packet is LoginPacket) packet.username = packet.username.replace(' ', '_')
+        if (packet is LoginPacket) {
+            if (!(plugin.config.get("namefixer") as Boolean)) return
+            packet.username = packet.username.replace(' ', '_')
+        }
     }
 }
--- src/main/kotlin/inkohx/xyz/nukkit/kotori/Main.kt
+++ src/main/kotlin/inkohx/xyz/nukkit/kotori/Main.kt
@@ -3,9 +3,13 @@ package inkohx.xyz.nukkit.kotori
 import cn.nukkit.plugin.PluginBase
 
 class Main : PluginBase() {
+    override fun onLoad() {
+        this.saveDefaultConfig()
+    }
+
     override fun onEnable() {
         server.pluginManager.registerEvents(EventListener(this), this)
-        logger.notice("ロード :)")
+        if (config.get("enable") as Boolean) logger.notice("ロード :)") else server.pluginManager.disablePlugin(this)
     }
 
     override fun onDisable() {
