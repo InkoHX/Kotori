7906190881fac4ff36841355c9ed57c0926424c8
InkoHX <inkohx@gmail.com>
2018-10-11 05:10:16 +0900

    Update src/main/kotlin/inkohx/xyz/nukkit/kotori/Main.kt

--- src/main/kotlin/inkohx/xyz/nukkit/kotori/Main.kt
+++ src/main/kotlin/inkohx/xyz/nukkit/kotori/Main.kt
@@ -1,18 +1,35 @@
 package inkohx.xyz.nukkit.kotori
 
 import cn.nukkit.plugin.PluginBase
+import inkohx.xyz.nukkit.kotori.serverlist.ServerListAPI
+import inkohx.xyz.nukkit.kotori.serverlist.ServerListTask
 
 class Main : PluginBase() {
+
+    internal val api = ServerListAPI()
+
     override fun onLoad() {
         this.saveDefaultConfig()
     }
 
     override fun onEnable() {
+        if (config.get("plugin-enable") as Boolean) logger.notice("Kotoriをロード :)") else server.pluginManager.disablePlugin(this)
         server.pluginManager.registerEvents(EventListener(this), this)
-        if (config.get("enable") as Boolean) logger.notice("ロード :)") else server.pluginManager.disablePlugin(this)
+        ServerList()
     }
 
     override fun onDisable() {
+        if (!(config.get("serverlist-enable") as Boolean)) logger.notice(api.logout())
         logger.notice("アンロード :(")
+    }
+
+    fun ServerList() {
+        if (config.get("serverlist-enable") as Boolean) {
+            logger.notice(api.login(config.get("serverlist-token").toString()))
+            server.scheduler.scheduleRepeatingTask(ServerListTask(this), 20)
+        } else {
+            logger.warning("ServerListは使わない設定になっています。")
+        }
+
     }
 }
