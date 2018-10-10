585e3245b43b006ce065a6e05718c10151f0f199
InkoHX <inkohx@gmail.com>
2018-10-11 05:10:12 +0900

    Update src/main/kotlin/inkohx/xyz/nukkit/kotori/EventListener.kt

--- src/main/kotlin/inkohx/xyz/nukkit/kotori/EventListener.kt
+++ src/main/kotlin/inkohx/xyz/nukkit/kotori/EventListener.kt
@@ -2,6 +2,8 @@ package inkohx.xyz.nukkit.kotori
 
 import cn.nukkit.event.EventHandler
 import cn.nukkit.event.Listener
+import cn.nukkit.event.player.PlayerJoinEvent
+import cn.nukkit.event.player.PlayerQuitEvent
 import cn.nukkit.event.server.DataPacketReceiveEvent
 import cn.nukkit.network.protocol.LoginPacket
 
@@ -10,8 +12,18 @@ class EventListener(private val plugin: Main) : Listener {
     fun Receive(event: DataPacketReceiveEvent) {
         val packet = event.packet
         if (packet is LoginPacket) {
-            if (!(plugin.config.get("namefixer") as Boolean)) return
+            if (!(plugin.config.get("player-namefix") as Boolean)) return
             packet.username = packet.username.replace(' ', '_')
         }
+    }
+
+    @EventHandler
+    fun Join(event: PlayerJoinEvent) {
+        if (!(plugin.config.get("serverlist-enable") as Boolean)) plugin.api.updatePlayers("join")
+    }
+
+    @EventHandler
+    fun Quit(event: PlayerQuitEvent) {
+        if (!(plugin.config.get("serverlist-enable") as Boolean)) plugin.api.updatePlayers("quit")
     }
 }
