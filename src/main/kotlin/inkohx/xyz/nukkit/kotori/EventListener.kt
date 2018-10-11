249e116c0d8a625e65e3c427785bcb4e958c1efa
InkoHX <inkohx@gmail.com>
2018-10-11 17:30:48 +0900

    Add bossbar and Fix Lag

--- src/main/kotlin/inkohx/xyz/nukkit/kotori/EventListener.kt
+++ src/main/kotlin/inkohx/xyz/nukkit/kotori/EventListener.kt
@@ -3,9 +3,11 @@ package inkohx.xyz.nukkit.kotori
 import cn.nukkit.event.EventHandler
 import cn.nukkit.event.Listener
 import cn.nukkit.event.player.PlayerJoinEvent
+import cn.nukkit.event.player.PlayerLoginEvent
 import cn.nukkit.event.player.PlayerQuitEvent
 import cn.nukkit.event.server.DataPacketReceiveEvent
 import cn.nukkit.network.protocol.LoginPacket
+import cn.nukkit.utils.DummyBossBar
 
 class EventListener(private val plugin: Main) : Listener {
     @EventHandler
@@ -20,10 +22,17 @@ class EventListener(private val plugin: Main) : Listener {
     @EventHandler
     fun Join(event: PlayerJoinEvent) {
         if (!(plugin.config.get("serverlist-enable") as Boolean)) plugin.api.updatePlayers("join")
+        event.player.createBossBar(DummyBossBar.Builder(event.player).length(100F).text("§l§aサバイバル").color(255, 255, 0).build())
     }
 
     @EventHandler
     fun Quit(event: PlayerQuitEvent) {
         if (!(plugin.config.get("serverlist-enable") as Boolean)) plugin.api.updatePlayers("quit")
+    }
+
+    @EventHandler
+    fun Login(event: PlayerLoginEvent) {
+        val player = event.player
+        player.setCheckMovement(false)
     }
 }
--- src/main/kotlin/inkohx/xyz/nukkit/kotori/survival/SurvivalCore.kt
+++ src/main/kotlin/inkohx/xyz/nukkit/kotori/survival/SurvivalCore.kt
@@ -1,9 +1,44 @@
 package inkohx.xyz.nukkit.kotori.survival
 
 import cn.nukkit.Player
+import cn.nukkit.Server
+import cn.nukkit.event.EventHandler
+import cn.nukkit.event.Listener
+import cn.nukkit.event.block.BlockBreakEvent
+import cn.nukkit.event.entity.EntityLevelChangeEvent
+import cn.nukkit.event.player.PlayerJoinEvent
+import cn.nukkit.item.Item
+import cn.nukkit.level.Position
+import java.util.*
 
-class SurvivalCore {
-    fun kit(player: Player) {
+class SurvivalCore : Listener {
+    fun RandomFood(player: Player) {
+        if (player.level.name == "world") return
+        if (Random().nextInt(21) == Random().nextInt(21)) {
+            player.inventory.addItem(Item(Item.STEAK, 0, 1))
+            player.sendTip("§l§aブロックからステーキが出てきた！たまげたなぁ")
+        }
 
+    }
+
+    @EventHandler
+    fun LevelChange(event: EntityLevelChangeEvent) {
+        val entity = event.entity
+        if (entity is Player) {
+            if (event.target.name == "survival") {
+                entity.teleport(Position(Random().nextInt(1000).toDouble(), 255.0, Random().nextInt(1000).toDouble(), Server.getInstance().getLevelByName("survival")))
+                entity.sendTitle("§aSTART", "§aSurvival §bNetwork", 40, 60, 40)
+            }
+        }
+    }
+
+    @EventHandler
+    fun BlockBreak(event: BlockBreakEvent) {
+        val player = event.player
+        RandomFood(player)
+    }
+
+    @EventHandler
+    fun PlayerJoin(event: PlayerJoinEvent) {
     }
 }
